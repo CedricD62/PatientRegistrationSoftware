7021681843eb8475b26252e7217194e4353b76cf
final <c.debaisieux@laposte.net>
2020-06-29 18:07:18 +0200

    change URL of externalFiles

--- patient_Informatise/src/com/filesActions/ReadExternalFiles.java
+++ patient_Informatise/src/com/filesActions/ReadExternalFiles.java
@@ -113,7 +113,7 @@ public class ReadExternalFiles {
 	private static void mailEndings() {
 		
 		try {
-			file = new File("C:/Users/final/Desktop/externalFiles/mailTerminaisons.csv");
+			file = new File("externalFiles/mailTerminaisons.csv");
 			fileR = new FileReader(file); 
 			buffer = new BufferedReader(fileR);
 			String line;
@@ -160,7 +160,7 @@ public class ReadExternalFiles {
 	private static void areaCode() {
 		
 		try {
-			file = new File("C:/Users/final/Desktop/externalFiles/areaCode.csv");
+			file = new File("externalFiles/areaCode.csv");
 			fileR = new FileReader(file); 
 			buffer = new BufferedReader(fileR);
 			String line;
